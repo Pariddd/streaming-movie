19335cdc41ffb7f289519d047e41e491bc34d0c0
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2025-07-04 12:51:56 +0700

    membuat middleware serta redirect after registration

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class SubscribeController extends Controller
+class SubscribeController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [
+            'auth',
+        ];
+    }
+
     public function showPlans()
     {
         $plans = Plan::all();
