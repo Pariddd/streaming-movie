fa4bc7ab4d3c05921ed66b5828a56983874b34de
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2025-07-08 00:36:20 +0700

    membuat fitur logout dengan menghapus devicelimit serta memperbaiki codingan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\SubscribeController;
+use App\Http\Middleware\RemoveDeviceBeforeLogout;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +16,9 @@ Route::get('/subscribe/success', [SubscribeController::class, 'showSuccess'])->n
 
 Route::get('/home', function () {
     return view('home');
-})->middleware(['auth', 'check.device'])->name('home');
+})->middleware(['auth', 'check.device.limit'])->name('home');
+
+Route::post('/logout', function (Request $request) {
+    // Laravel Fortify menangani logout, kita hanya tambahkan middleware
+    return app(\Laravel\Fortify\Http\Controllers\AuthenticatedSessionController::class)->destroy($request);
+})->middleware(['auth', 'logout.device'])->name('logout');
